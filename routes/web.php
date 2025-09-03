b7de1372ead38717ca49b412d95d8f489e869abc
AbdiCytus <abdiprayuda203@gmail.com>
2025-09-03 15:31:01 +0800

    bug fix rute receive-user

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 use OpenSynergic\LaravelSSO\Controllers\UserApiReceiveController;
 use OpenSynergic\LaravelSSO\Controllers\UserReceiveController;
 
-Route::get('receive-user', [UserReceiveController::class, 'userReceive']);
+Route::middleware(['web'])->group(function () {
+    Route::get('receive-user', [UserReceiveController::class, 'userReceive']);
+});
 Route::get('user/{id?}', [UserApiReceiveController::class, 'users']);
